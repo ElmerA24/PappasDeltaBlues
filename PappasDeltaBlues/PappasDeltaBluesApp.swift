f5f009bf0f5ab78447bc967198239ff28b97d337
Elmer Arce <158310820+ElmerA24@users.noreply.github.com>
2025-01-19 13:22:14 -0600

    Initial Commit

---
+++ PappasDeltaBlues/PappasDeltaBluesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PappasDeltaBluesApp.swift
+//  PappasDeltaBlues
+//
+//  Created by Elmer Arce on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct PappasDeltaBluesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
